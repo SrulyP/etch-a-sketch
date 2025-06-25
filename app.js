06dca0b2ab3caf3c6c0afa4122e3806e67bd2c68
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-06-25 23:25:19 +0300

    Add logic to change the size of the grid, add button to request size change

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-const container = document.querySelector(".grid");
+let x = 16;
+let y = 16;
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++){
+const container = document.querySelector(".grid");
+const changeSizeButton = document.querySelector(".sizeButton");
+
+changeSizeButton.onclick = (e) => {
+    let value = prompt("Input the number of squares per side for the new grid: ");
+    x, y = value;
+};
+
+for (let i = 0; i < x; i++) {
+    for (let j = 0; j < y; j++){
         const gridPiece = document.createElement("div");
 
         gridPiece.setAttribute("class", "grid-piece");
-        gridPiece.setAttribute("style", "width: 20px; height: 20px;");
+        gridPiece.setAttribute("style", "width: 20px; height: 20px; background-color: black; opacity: 0;");
+        gridPiece.addEventListener('mouseover', () => {
+            increaseOpacity(gridPiece);
+        });
         container.appendChild(gridPiece);
     }   
 }
 
+function increaseOpacity(gridPiece) {
+    let opacity = parseFloat(gridPiece.style.opacity);
+    gridPiece.style.opacity = opacity + 0.1;
+}
 
-container.addEventListener('mousemove', (e) => {
-    
-});
+
