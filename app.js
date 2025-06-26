f102b8caaeb5055a87589c4a58043b6d85d65845
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-06-26 12:26:30 +0300

    Add separation between sections of code, and fix resetGrid function to erase the current board, as well as fix the height and width of each square to match and fit the grid

--- app.js
+++ app.js
@@ -1,30 +1,63 @@
-let x = 16;
-let y = 16;
+// ----------------- x ----------------- 
+let value = 16;
 
-const container = document.querySelector(".grid");
-const changeSizeButton = document.querySelector(".sizeButton");
+let grid = document.querySelector(".grid");
+const sizeButton = document.querySelector(".sizeButton");
+const container = document.querySelector(".container");
 
-changeSizeButton.onclick = (e) => {
-    let value = prompt("Input the number of squares per side for the new grid: ");
-    x, y = value;
-};
 
-for (let i = 0; i < x; i++) {
-    for (let j = 0; j < y; j++){
-        const gridPiece = document.createElement("div");
+// ----------------- Change Size Button -----------------
 
-        gridPiece.setAttribute("class", "grid-piece");
-        gridPiece.setAttribute("style", "width: 20px; height: 20px; background-color: black; opacity: 0;");
-        gridPiece.addEventListener('mouseover', () => {
-            increaseOpacity(gridPiece);
-        });
-        container.appendChild(gridPiece);
-    }   
+sizeButton.addEventListener("click", () => {
+    console.log("button clicked");
+    while (value > 100 || value < 16) {
+        console.log("requesting val");
+        value = Number(prompt("Input the number of squares per side for the new grid (16 - 100): "));
+    }
+    console.log("val accepted, resetting grid");
+    resetGrid(value);
+});
+
+
+// ----------------- Reset Grid Function -----------------
+
+function resetGrid(value){
+    container.removeChild(grid);
+    console.log("removed grid");
+    grid = document.createElement("div");
+    grid.setAttribute("class", "grid");
+    console.log("created new grid element");
+    container.appendChild(grid);
+    console.log("appended grid to container");
+
+
+    let x = value;
+    let y = value;
+    let squareHeight = 320/x;
+    console.log(`value = ${value}, squareHeight = ${squareHeight}`);
+
+    let styles = `height: ${squareHeight}px; width: ${squareHeight}px; background-color: black; opacity: 0;`
+
+    for (let i = 0; i < x; i++) {
+        for (let j = 0; j < y; j++){
+            const gridPiece = document.createElement("div");
+
+            gridPiece.setAttribute("class", "grid-piece");
+            gridPiece.setAttribute("style", styles);
+            gridPiece.addEventListener('mouseover', () => {
+                increaseOpacity(gridPiece);
+            });
+            grid.appendChild(gridPiece);
+        }   
+    }
 }
+resetGrid(value);
+
+
+// ----------------- Increase Opacity Function -----------------
 
 function increaseOpacity(gridPiece) {
     let opacity = parseFloat(gridPiece.style.opacity);
     gridPiece.style.opacity = opacity + 0.1;
 }
-
 
