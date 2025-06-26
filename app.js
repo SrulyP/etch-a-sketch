15cba38701616f42c0df0bd0c1a9f4ef061fb706
Israel Polasak <146323213+SrulyP@users.noreply.github.com>
2025-06-26 13:12:32 +0300

    Add functionality for the reset, and colormode buttons; organize the functions and comments

--- app.js
+++ app.js
@@ -1,42 +1,71 @@
-// ----------------- x ----------------- 
+// ============================== // Initial and Global Values //  ============================== //
+
 let value = 16;
+const gridHeight = 500;
+let mode = "color";
+let hexColor;
 
 let grid = document.querySelector(".grid");
 const sizeButton = document.querySelector(".sizeButton");
+const colorButton = document.querySelector(".colorButton");
+const grayButton = document.querySelector(".grayButton");
+const resetButton = document.querySelector(".resetButton");
 const container = document.querySelector(".container");
 
 
-// ----------------- Change Size Button -----------------
+// ============================== // Buttons // ============================== //
+
+// ----------- Change Size Button -----------
 
 sizeButton.addEventListener("click", () => {
-    console.log("button clicked");
+    value = 0;
     while (value > 100 || value < 16) {
-        console.log("requesting val");
         value = Number(prompt("Input the number of squares per side for the new grid (16 - 100): "));
     }
-    console.log("val accepted, resetting grid");
     resetGrid(value);
 });
 
 
+// ----------- Change to Color Mode Button -----------
+
+colorButton.addEventListener("click", () => {
+    mode = "color";
+    resetGrid(value, mode);
+});
+
+
+// ----------- Change to Grayscale Mode Button -----------
+
+grayButton.addEventListener("click", () => {
+    mode = "gray";
+    resetGrid(value, mode);
+});
+
+
+// ----------- Reset Grid Button -----------
+
+resetButton.addEventListener("click", () => {
+    resetGrid(value, mode);
+});
+
+
+// ============================== // Functions // ============================== //
+
 // ----------------- Reset Grid Function -----------------
 
 function resetGrid(value){
+    // Remove the current grid and create a new one
     container.removeChild(grid);
-    console.log("removed grid");
     grid = document.createElement("div");
     grid.setAttribute("class", "grid");
-    console.log("created new grid element");
     container.appendChild(grid);
-    console.log("appended grid to container");
 
 
     let x = value;
     let y = value;
-    let squareHeight = 320/x;
-    console.log(`value = ${value}, squareHeight = ${squareHeight}`);
+    let squareHeight = gridHeight/x;
 
-    let styles = `height: ${squareHeight}px; width: ${squareHeight}px; background-color: black; opacity: 0;`
+    let styles = `height: ${squareHeight}px; width: ${squareHeight}px;`
 
     for (let i = 0; i < x; i++) {
         for (let j = 0; j < y; j++){
@@ -45,7 +74,12 @@ function resetGrid(value){
             gridPiece.setAttribute("class", "grid-piece");
             gridPiece.setAttribute("style", styles);
             gridPiece.addEventListener('mouseover', () => {
-                increaseOpacity(gridPiece);
+                if (mode == "gray") {
+                    increaseOpacity(gridPiece);
+                }
+                else {
+                    randomColors(gridPiece);
+                }
             });
             grid.appendChild(gridPiece);
         }   
@@ -57,7 +91,16 @@ resetGrid(value);
 // ----------------- Increase Opacity Function -----------------
 
 function increaseOpacity(gridPiece) {
+    gridPiece.style.backgroundColor = "#000000";
+    gridPiece.style.opacity = 0;
     let opacity = parseFloat(gridPiece.style.opacity);
     gridPiece.style.opacity = opacity + 0.1;
 }
 
+
+// ----------------- Random Colors Function -----------------
+
+function randomColors(gridPiece) {
+    hexColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    gridPiece.style.backgroundColor = hexColor;
+}
